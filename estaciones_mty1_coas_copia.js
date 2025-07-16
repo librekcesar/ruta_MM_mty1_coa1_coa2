afa393bb6b3cd19fd4465c4f09536b5b87889c57
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-16 10:56:59 -0600

    Add files via upload

---
+++ estaciones_mty1_coas_copia.js
@@ -0,0 +1,10 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mty1_coas",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "PM6R+9HX", "description": "MTY1", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -100.3085156, 25.7109875, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Blvd. Vito Alessio Robles 2075, Sin Nombre de Col 25, 25023 Saltillo, Coah.", "description": "XCOA1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -101.0049366, 25.4507053, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Av Morelos 703, Primero de Cobián Centro, 27000 Torreón, Coah.", "description": "XCOA2", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -103.4569511, 25.5393478, 0.0 ] } }
+]
+}
